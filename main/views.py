5a4e5782c7ccc5a18a358e266c5e1fc0f1461f7c
h4cktivist <popovvadim0605@gmail.com>
2021-06-22 13:15:16 +0500

    feat: users on index page with pagination add

--- main/views.py
+++ main/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
+from django.contrib.auth.models import User
+from django.db.models import Q
 
 
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        search_data = request.POST.get('search')
+        users = User.objects.filter(Q(first_name__contains=search_data) | Q(last_name__contains=search_data))
+    else:
+        users = User.objects.all()
+
+    paginator = Paginator(users, 6)
+    page = request.GET.get('page', 1)
+    page_users = paginator.get_page(page)
+
+    return render(request, 'index.html', {'users': page_users})
